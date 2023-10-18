1e529f913828c7f78da14e44e1c00f6335c62ad0
parth108 <76932381+ParthNikam@users.noreply.github.com>
2023-10-18 21:36:13 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 // client/src/api/index.js
 import axios from 'axios';
 
-const baseURL = 'http://127.0.0.1:5000'; // Replace with your actual base API URL
-// const baseURL = "https://trym-backend.onrender.com"
+const baseURL = 'https://testbackend-brgm.onrender.com'; // Replace with your actual base API URL
+
 
 const api = axios.create({
   baseURL,
